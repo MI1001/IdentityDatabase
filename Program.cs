e122b75a28a1eab3dd0368b37019bb7aef5ab2a5
Maxim Ivanov <my.a.1.b.2.c.3.mail@gmail.com>
2023-08-03 05:31:07 +0300

    Redact #1 | 08/03/2023

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace Nuget.IdentityDatabase
 {
@@ -9,62 +10,130 @@ namespace Nuget.IdentityDatabase
         private static void Main(string[] args)
         {
             string path = Path.Combine("indices.dat");
-            string configurationPath = Path.Combine("Config.json");
+            string configurationPath = Path.Combine("Configuration.json");
 
+            ContentJSON contentJSON = new();
             Сomposition comp1 = new()
             {
-                Multiplier_A = "Blender 1",
-                Multiplier_B = "Blender 2"
+                Multiplier_A = "Socrates is a man",
+                Multiplier_B = "All men are mortal", // man are mortal
+                Premise = { }
             };
 
-            string json = JsonConvert.SerializeObject(comp1, Formatting.Indented);
+            TrueStatement trueStatement = new()
+            {
+                Proposition = "Socrates is mortal"
+            };
+
+            string json = JsonConvert.SerializeObject(comp1, Formatting.Indented);            
 
             // Copyright
             Console.WriteLine("{0} | Identity Database | INFO | IDB | InitCore() Copyright © 2023 WG Element.",
                 new DateTime(2023, 8, 2, 0, 0, 0, DateTimeKind.Utc));
 
-            try
+            if (File.Exists(path) && File.Exists(configurationPath))
             {
-                if (File.Exists(path) && File.Exists(configurationPath))
-                {
-                    Console.WriteLine("{0} | Identity Database | INFO | IDB | Файл *.dat ({1}) уже существует. Конфигурационный файл ({2}) присутствует.",
-                        DateTime.UtcNow, path, configurationPath);
+                Console.WriteLine("{0} | Identity Database | INFO | IDB | Файл *.dat ({1}) уже существует. Конфигурационный файл ({2}) присутствует.",
+                    DateTime.UtcNow, path, configurationPath);
 
-                    // Continue...
-                }
-                else
+                // Continue...
+
+
+            }
+            else
+            {
+                if (!File.Exists(path))
                 {
                     StreamWriter sw = File.CreateText(path);
 
                     sw.Close();
                 }
             }
-            catch (Exception e)
-            {
-                Console.WriteLine("Ошибка: " + e.Message);
-            }
+
+            Сomposition config_defaultGet = JsonConvert.DeserializeObject<Сomposition>(json);
+            Сomposition configGet = JsonConvert.DeserializeObject<Сomposition>(contentJSON.Get_JSON(configurationPath, ""));
+
+            Console.WriteLine(config_defaultGet.Multiplier_A);
+            Console.WriteLine(configGet.Multiplier_A);
+            _ = Console.ReadLine();
+        }
+
+        
+    }
+
+    // Error.
+
+    /// <summary>
+    /// error
+    /// </summary>
+    public partial class P_API_ERROR { }
+
+    // JSON
+
+    /// <summary>
+    /// Работа с JSON.
+    /// </summary>
+    public partial class ContentJSON
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="lacal path"></param>
+        /// <param name="token"></param>
+        /// <returns></returns>
+        public string Get_JSON(string lacal_path, string token)
+        {
+            return ParseJSON_SelectToken(
+                GetResponse_TypeJSON(lacal_path),
+                token);
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="local path"></param>
+        /// <returns></returns>
+        private string GetResponse_TypeJSON(string lacal_path)
+        {
+            string result;
 
             try
             {
-                using (StreamReader sr = new(configurationPath))
-                {
-                    string line;
-
-                    while ((line = sr.ReadLine()) != null)
-                    {
-                        Console.WriteLine(line);
-                    }
-                }
+                using StreamReader sr = new(lacal_path);
+                return (result = sr.ReadToEnd());
             }
             catch (Exception e)
             {
                 // Let the user know what went wrong.
                 Console.WriteLine("The configuration file could not be read:");
                 Console.WriteLine(e.Message);
-            }            
 
-            Console.WriteLine(json);
-            Console.ReadLine();
+                return result = string.Empty;
+            }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="content">JSON content</param>
+        /// <param name="jToken">JSON Node</param>
+        /// <returns></returns>
+        private string ParseJSON_SelectToken(string content, string jToken)
+        {
+            JObject obj = JObject.Parse(content);
+
+            string data = string.Empty;
+
+            try
+            {
+                data = obj.SelectToken(jToken).ToString();
+            }
+            catch
+            {
+                //do something
+            }
+
+            return data;
         }
     }
 
@@ -82,6 +151,22 @@ namespace Nuget.IdentityDatabase
         /// 
         /// </summary>
         public string? Multiplier_B { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public TrueStatement? Premise { get; set; }
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    public class TrueStatement
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        public string? Proposition { get; set; }
     }
 
     /// <summary>
